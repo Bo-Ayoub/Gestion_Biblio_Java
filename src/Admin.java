d28a32d97f30f945d6b15c4f8a5a31dd6ca11153
Bo-Ayoub <BOUSSAIDI-AYOUB@hotmail.com>
2023-05-24 12:53:00 +0200

    add Admin Class

---
+++ src/Admin.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Admin extends Personne {
+
+	public Admin() {
+		super();
+	}
+	public Admin(String Nom,String Prenom,String NumeroTelephone,String CIN,Date DateNaissance) {
+		super(Nom,Prenom,NumeroTelephone,CIN,DateNaissance);
+	}
+}
